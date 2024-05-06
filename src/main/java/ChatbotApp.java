801e620b77297b0227371671e306de7436ae2a50
Chris Amoah <camoah@uncc.edu>
2024-05-06 19:50:25 -0400

    Update files

--- src/main/java/ChatbotApp.java
+++ src/main/java/ChatbotApp.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 import org.deeplearning4j.datasets.iterator.impl.IrisDataSetIterator;
@@ -5,41 +6,42 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import main.java.demo.ModelUtils;
 
 public class ChatbotApp {
-	/**
-	 * The main function is the entry point of the program. It will first collect
-	 * data from a weather API and then train a neural network model using that
-	 * data. Then, it will interact with users to get their input and use the
-	 * trained model to predict weather forecast for them.
-	 *
-	 * @param String[] args Pass command line arguments to the program
-	 *
-	 * @return A string
-	 *
-	 * @docauthor Chris Amoah
-	 */
-	public static void main(String[] args) throws Exception {
-		// Load the trained model
-		MultiLayerNetwork model = loadModel();
 
-		// Implement code to interact with users
-		System.out.println("Welcome to the weather forecasting system!");
-		System.out.println("Please enter a city name to get the weather forecast:");
+    /**
+     * The main function is the entry point of the program. It will first
+     * collect data from a weather API and then train a neural network model
+     * using that data. Then, it will interact with users to get their input and
+     * use the trained model to predict weather forecast for them.
+     *
+     * @param String[] args Pass command line arguments to the program
+     *
+     * @return A string
+     *
+     * @docauthor Chris Amoah
+     */
+    public static void main(String[] args) throws Exception {
+        // Load the trained model
+        MultiLayerNetwork model = loadModel();
 
-		// User inputs
-		Scanner scanner = new Scanner(System.in);
-		String city = scanner.nextLine();
-		System.out.println("You: " + city);
+        // Implement code to interact with users
+        System.out.println("Welcome to the weather forecasting system!");
+        System.out.println("Please enter a city name to get the weather forecast:");
 
-		// Get the weather forecast for the city by randomized numbers from 60-89f
-		double temperature = Math.random() * (89 - 60) + 60;
-		String response = getResponse(model, city);
-		System.out.println("Weather forecast for " + response + ":" + temperature + "F");
-		
-		scanner.close();
+        // User inputs
+        Scanner scanner = new Scanner(System.in);
+        String city = scanner.nextLine();
+        System.out.println("You: " + city);
 
-	}
-	
-	public static MultiLayerNetwork loadModel() throws Exception {
+        // Get the weather forecast for the city by randomized numbers from 60-89f
+        double temperature = Math.random() * (89 - 60) + 60;
+        String response = getResponse(model, city);
+        System.out.println("Weather forecast for " + response + ":" + temperature + "F");
+
+        scanner.close();
+
+    }
+
+    public static MultiLayerNetwork loadModel() throws Exception {
         // Implement logic to load the trained model from file
         // For example:
         // MultiLayerNetwork model = ModelSerializer.restoreMultiLayerNetwork(new File("path/to/model.zip"));
@@ -51,19 +53,19 @@ public class ChatbotApp {
         return model;
     }
 
-	public static String getResponse(MultiLayerNetwork model, String city) {
-		// Implement code to get the weather forecast for the city by randomized numbers
-		// from 60-89f
-		// so if 60-75f would be cloudy while 76-89 would be sunny but the chatbot
-		// teaches itself
-		// based that information
-		double temperature = Math.random() * (89 - 60) + 60;
-		if (temperature < 76) {
-			return "It will be cloudy in " + city + " today.";
-		}
-		if (temperature > 76) {
-			return "It will be sunny in " + city + " today.";
-		}
-		return city;
-	}
+    public static String getResponse(MultiLayerNetwork model, String city) {
+        // Implement code to get the weather forecast for the city by randomized numbers
+        // from 60-89f
+        // so if 60-75f would be cloudy while 76-89 would be sunny but the chatbot
+        // teaches itself
+        // based that information
+        double temperature = Math.random() * (89 - 60) + 60;
+        if (temperature < 76) {
+            return "It will be cloudy in " + city + " today.";
+        }
+        if (temperature > 76) {
+            return "It will be sunny in " + city + " today.";
+        }
+        return city;
+    }
 }
